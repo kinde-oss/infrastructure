a14815fcf210b13193116205fcd678b55ab88f3e
dtoxvanilla1991 <dtoxvanilla@gmail.com>
2026-08-26 00:10:25 +0100

    fix: await remaining unawaited rejects assertions in createKindeAPI error tests

--- lib/main.test.ts
+++ lib/main.test.ts
@@ -522,16 +522,16 @@ describe("createKindeAPI", () => {
   });
 
   describe("Error handling", () => {
-    it("Missing client ID", () => {
+    it("Missing client ID", async () => {
       global.kinde.env.get.mockReturnValueOnce(undefined);
-      expect(
+      await expect(
         async () => await createKindeAPI({ ...mockEvent, context: {} }),
       ).rejects.toThrowError("M2M client ID not set");
     });
-    it("Missing client Secret", () => {
+    it("Missing client Secret", async () => {
       global.kinde.env.get.mockReturnValueOnce({ value: "test" });
       global.kinde.env.get.mockReturnValueOnce(undefined);
-      expect(
+      await expect(
         async () => await createKindeAPI({ ...mockEvent, context: {} }),
       ).rejects.toThrowError("M2M client secret not set");
     });
